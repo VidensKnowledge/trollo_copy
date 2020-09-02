659749edfe239883c00ec18aac4d44ca20b3cd90
VidensKnowledge <devydoolander@gmail.com>
2020-09-02 15:18:40 -0600

    devise views and models set

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root "boards#index"
   devise_for :users
+
+
+  resources :boards
+  # resources :lists
+  # resources :tasks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
