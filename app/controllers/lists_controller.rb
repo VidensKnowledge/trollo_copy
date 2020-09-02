92c2ea2c2f02d7b88656216d7fcbe15e57345444
VidensKnowledge <devydoolander@gmail.com>
2020-09-02 16:12:33 -0600

    correction on lists controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,8 +1,24 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [ :show, :edit, :update, :destroy ]
+
   def index
+    @lists = current_user.boards.lists
   end
 
   def new
+    @list = List.new
+  end
+
+  def create
+    @list = current_user.boards.lists.new(sword_params)
+
+    if @list.save
+    flash[:success] = "List Created"
+      redirect_to lists_path
+    else
+      flash[:error] = "error #{@sword.errors.full_messages.join('\n')}"
+      render :new
+    end
   end
 
   def show
@@ -10,4 +26,28 @@ class ListsController < ApplicationController
 
   def edit
   end
+
+  def update
+   if @list.update(list_params)
+      redirect_to lists_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_path
+  end
+
+  private
+
+  def set_list
+    @list = current_user.boards.lists.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   def index
+    # @tasks = Task.
   end
 
   def new
